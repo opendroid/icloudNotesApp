bc7e8e3043e81b0ef679c3c1317b841083b4124c
opendriod <ajaythakur@hotmail.com>
2016-06-25 12:01:07 -0700

    commit projects skeleton

---
+++ icloudNotes-macOS/icloudNotes-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  icloudNotes-macOS
+//
+//  Created by Ajay Thakur on 6/25/16.
+//  Copyright © 2016 Ajay Thakur. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ icloudNotes-macOS/icloudNotes-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  icloudNotes-macOS
+//
+//  Created by Ajay Thakur on 6/25/16.
+//  Copyright © 2016 Ajay Thakur. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
